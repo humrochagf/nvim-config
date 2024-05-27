6b0872e2da8214eaed4032dff5da2c58248410fa
Humberto Rocha <humrochagf@gmail.com>
2024-05-27 14:08:26 -0400

    Tab navigation

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -12,7 +12,7 @@ return {
   {
     "hrsh7th/nvim-cmp",
     config = function()
-      local cmp = require("cmp")
+      local cmp, luasnip = require("cmp"), require("luasnip")
       require("luasnip.loaders.from_vscode").lazy_load()
 
       cmp.setup({
@@ -31,6 +31,24 @@ return {
           ["<C-Space>"] = cmp.mapping.complete(),
           ["<C-e>"] = cmp.mapping.abort(),
           ["<CR>"] = cmp.mapping.confirm({ select = true }),
+          ["<Tab>"] = function(fallback)
+            if cmp.visible() then
+              cmp.select_next_item()
+            elseif luasnip.expand_or_jumpable() then
+              luasnip.expand_or_jump()
+            else
+              fallback()
+            end
+          end,
+          ["<S-Tab>"] = function(fallback)
+            if cmp.visible() then
+              cmp.select_prev_item()
+            elseif luasnip.jumpable(-1) then
+              luasnip.jump(-1)
+            else
+              fallback()
+            end
+          end,
         }),
         sources = cmp.config.sources({
           { name = "nvim_lsp" },
