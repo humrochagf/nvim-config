6e4f655ce2627dc7c94dc00277777ee8df03efd9
Humberto Rocha <humrochagf@gmail.com>
2024-06-02 21:43:57 -0400

    Set more confort options

--- lua/configs.lua
+++ lua/configs.lua
@@ -1,10 +1,8 @@
 local default_options = {
-  -- tab configs
-  expandtab = true,
-  tabstop = 2,
-  softtabstop = 2,
-  shiftwidth = 2,
-
+  expandtab = true, -- tabs are spaces, not tabs
+  tabstop = 2, -- an indentation every 2 spaces
+  softtabstop = 2, -- let backspace delete indent
+  shiftwidth = 2, -- use indents of 2 spaces
   listchars = "tab:|-,trail:.,extends:>,precedes:<", -- Map special characters
   backup = false, -- creates a backup file
   swapfile = false, -- disable swapfile
@@ -13,9 +11,14 @@ local default_options = {
   spelllang = { "en", "pt_br", "fr" },
   number = true, -- numbered lines
   ignorecase = true, -- ignore case in search patterns
-  smartcase = true,
+  smartcase = true, -- case sensitive when Upper Case is present
+  autoindent = true -- indent at the same level of the previous line
 }
 
+-- move to other line with left right navigation at the line edges
+vim.opt.whichwrap:append "<,>,[,],h,l"
+
+-- Apply default_options
 for k, v in pairs(default_options) do
   vim.opt[k] = v
 end
