13cf3b4528bff5974abf83c6027ef06bbd05ba4b
Humberto Rocha <humrochagf@gmail.com>
2025-02-23 10:11:35 -0500

    Fix mypy on windows

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -11,7 +11,12 @@ return {
         null_ls.builtins.diagnostics.mypy.with({
           extra_args = function()
             local virtual = os.getenv("VIRTUAL_ENV") or os.getenv("CONDA_PREFIX") or "/usr"
-            return { "--python-executable", virtual .. "/bin/python3" }
+
+            if vim.g.os == "Windows" then
+              return { "--python-executable", virtual .. "/Scripts/python" }
+            else
+              return { "--python-executable", virtual .. "/bin/python3" }
+            end
           end,
         }),
       }
