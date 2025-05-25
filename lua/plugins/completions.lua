d7d85de55a5d298af4fd1af4ab56f4e085da86ba
Humberto Rocha <humrochagf@gmail.com>
2025-05-25 18:21:33 -0400

    More lsp configs

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -1,14 +1,19 @@
 return {
   {
     "saghen/blink.cmp",
-    dependencies = { "rafamadriz/friendly-snippets" },
+    dependencies = {
+      "rafamadriz/friendly-snippets",
+      "xzbdmw/colorful-menu.nvim",
+    },
     event = { "LspAttach" },
 
     version = "1.*",
 
     opts = {
       fuzzy = { implementation = "rust" },
-      keymap = { preset = "default" },
+      keymap = {
+        preset = "default",
+      },
       signature = {
         enabled = true,
         window = {
@@ -21,7 +26,17 @@ return {
         border = "rounded",
         winhighlight = "Normal:NormalFloat,FloatBorder:FloatBorder,CursorLine:PmenuSel,Search:None",
         draw = {
-          treesitter = { "lsp" },
+          columns = { { "kind_icon" }, { "label", gap = 1 } },
+          components = {
+            label = {
+              text = function(ctx)
+                return require("colorful-menu").blink_components_text(ctx)
+              end,
+              highlight = function(ctx)
+                return require("colorful-menu").blink_components_highlight(ctx)
+              end,
+            },
+          },
         },
       },
       documentation = {
