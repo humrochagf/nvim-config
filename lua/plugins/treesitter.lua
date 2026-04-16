c5e2b2040564277dbf6725f8a589520740c8f956
Humberto Rocha Gonçalves Filho <humberto.rocha-goncalves-filho@ubisoft.com>
2026-04-16 09:55:52 -0400

    Fix nvim.treesitter to support nvim 0.12

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,40 +1,55 @@
 return {
   "nvim-treesitter/nvim-treesitter",
+  lazy = false,
+  branch = "main",
   build = ":TSUpdate",
-  config = function()
-    local config = require("nvim-treesitter.configs")
+  main = "nvim-treesitter",
+  init = function()
+    vim.api.nvim_create_autocmd(
+      'FileType',
+      {
+        callback = function()
+          -- Enable treesitter highlighting and disable regex syntax
+          pcall(vim.treesitter.start)
+          -- Enable treesitter-based indentation
+          vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+        end,
+      }
+    )
 
-    config.setup({
-      ensure_installed = {
-        "bash",
-        "c",
-        "c_sharp",
-        "cpp",
-        "css",
-        "dockerfile",
-        "go",
-        "helm",
-        "http",
-        "javascript",
-        "json",
-        "lua",
-        "markdown",
-        "markdown_inline",
-        "python",
-        "regex",
-        "rust",
-        "terraform",
-        "toml",
-        "typescript",
-        "vim",
-        "yaml",
-        "zig",
-      },
-      highlight = { enable = true },
-      indent = {
-        enable = true,
-        disable = { "yaml", "python" },
-      },
-    })
-  end
+    local ensureInstalled = {
+      "bash",
+      "c",
+      "c_sharp",
+      "cpp",
+      "css",
+      "dockerfile",
+      "go",
+      "helm",
+      "http",
+      "javascript",
+      "json",
+      "lua",
+      "markdown",
+      "markdown_inline",
+      "python",
+      "regex",
+      "rust",
+      "terraform",
+      "toml",
+      "typescript",
+      "vim",
+      "yaml",
+      "zig",
+    }
+
+    local alreadyInstalled = require('nvim-treesitter.config').get_installed()
+
+    local parsersToInstall = vim.iter(ensureInstalled)
+      :filter(function(parser)
+        return not vim.tbl_contains(alreadyInstalled, parser)
+      end)
+      :totable()
+    require('nvim-treesitter').install(parsersToInstall)
+  end,
 }
