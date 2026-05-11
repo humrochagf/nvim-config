0f96cfeaa83e432c0314f601293b97b7ad72445d
Humberto Rocha <humrochagf@gmail.com>
2026-05-10 20:10:26 -0400

    Persistent undo and some ui improvements

--- lua/configs.lua
+++ lua/configs.lua
@@ -7,6 +7,7 @@ vim.opt.shiftwidth = 2                                    -- use indents of 2 sp
 vim.opt.listchars = "tab:|-,trail:.,extends:>,precedes:<" -- map special characters
 vim.opt.backup = false                                    -- creates a backup file
 vim.opt.swapfile = false                                  -- disable swapfile
+vim.opt.undofile = true                                   -- persistent undo across sessions
 vim.opt.clipboard = "unnamedplus"                         -- allows neovim to access the system clipboard
 vim.opt.wrap = false                                      -- display line without breaking into multiple
 vim.opt.spelllang = { "en", "pt_br", "fr" }               -- configured spell check languages
@@ -16,6 +17,8 @@ vim.opt.smartcase = true                                  -- case sensitive when
 vim.opt.autoindent = true                                 -- indent at the same level of the previous line
 vim.opt.scrolloff = 8                                     -- keep 8 lines around the cursor
 vim.opt.updatetime = 50                                   -- snappy CursorHold
+vim.opt.winborder = "rounded"                             -- global border setting
+vim.opt.fcs = 'eob: '                                     -- remove the ~ chars after the end of buffer
 
 -- Show invisible characters
 vim.cmd [[
